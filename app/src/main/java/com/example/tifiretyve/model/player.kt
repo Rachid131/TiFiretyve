6369241cc45fcdf2ffc9d655844429057e84fb1f
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-09-04 01:23:18 +0200

    made model view and viewmodel packages

---
+++ app/src/main/java/com/example/tifiretyve/model/player.kt
@@ -0,0 +1,5 @@
+package com.example.tifiretyve.model
+
+//holds lives of a player, their points, and which lifelines are used
+class player {
+}
---
+++ app/src/main/java/com/example/tifiretyve/model/questionBank.kt
@@ -0,0 +1,4 @@
+package com.example.tifiretyve.model
+
+class questionBank {
+}
