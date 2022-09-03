8f93e225c431a1f2b9a44c96bd7ed62330600add
Rachid Gaim Ahwaz <s183925@dtu.dk>
2022-09-04 01:58:35 +0200

    commenting. model first draft done

--- app/src/main/java/com/example/tifiretyve/model/Answer.kt
+++ app/src/main/java/com/example/tifiretyve/model/Answer.kt
@@ -1,5 +1,6 @@
 package com.example.tifiretyve.model
 
+//holds the string for an answer, and wether it is hidden
 class Answer(answerString: String, var isHidden: Boolean) {
     fun changeHidden(){
         isHidden = !isHidden
--- app/src/main/java/com/example/tifiretyve/model/Trial.kt
+++ app/src/main/java/com/example/tifiretyve/model/Trial.kt
@@ -1,5 +1,6 @@
 package com.example.tifiretyve.model
 
+//holds questions, possible answer, and definite answer given by int
 class Trial (val question: String, answer1: Answer, answer2: Answer, answer3: Answer, answer4: Answer, correctAnswer: Int ) {
 
 
